14225a76401a4b641b9a040259fdf6fa14eb46a7
Manuel Valles <manukempo@gmail.com>
2021-10-01 12:47:33 +0100

    Lock down Subscriptions

--- graphql-prisma/src/resolvers/Subscription.js
+++ graphql-prisma/src/resolvers/Subscription.js
@@ -1,6 +1,9 @@
+import getUserId from '../utils/getUserId';
+import { withFilter } from 'graphql-yoga';
+
 const Subscription = {
   comment: {
-    subscribe: async (parent, { postId }, { prisma, pubsub }, info) => {
+    subscribe: async (parent, { postId }, { prisma, pubsub }) => {
       const [post] = await prisma.post.findMany({
         where: {
           AND: [{ id: postId }, { published: true }],
@@ -12,8 +15,15 @@ const Subscription = {
       return pubsub.asyncIterator(`comment ${postId}`);
     },
   },
+  // ToDo: add myPost - current issue with null returned
   post: {
-    subscribe: (parent, args, { pubsub }, info) => pubsub.asyncIterator('post'),
+    subscribe: withFilter(
+      (parent, args, { pubsub }) => pubsub.asyncIterator('post'),
+      ({ post }, variables, { request }) => {
+        const userId = getUserId(request);
+        return post.data.authorId === userId;
+      }
+    ),
   },
 };
 
