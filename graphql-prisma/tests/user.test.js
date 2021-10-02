c7ee356436bfd29eb14912eb68996cebd9f4ccda
Manuel Valles <manukempo@gmail.com>
2021-10-02 14:20:12 +0100

    Test Queries: GetUsers & GetPosts

--- graphql-prisma/tests/user.test.js
+++ graphql-prisma/tests/user.test.js
@@ -63,3 +63,41 @@ test('Should create a new user', async () => {
 
   expect(existsUser).toBe(1);
 });
+
+test('Should expose public author profiles', async () => {
+  const getUsers = gql`
+    query {
+      users {
+        id
+        name
+        email
+      }
+    }
+  `;
+
+  const response = await client.query({ query: getUsers });
+
+  expect(response.data.users.length).toBe(1);
+  expect(response.data.users[0].email).toBe(null);
+  expect(response.data.users[0].name).toBe('Manu Kem');
+});
+
+test('Should expose published posts', async () => {
+  const getPosts = gql`
+    query {
+      posts {
+        id
+        title
+        body
+        published
+      }
+    }
+  `;
+
+  const response = await client.query({ query: getPosts });
+
+  expect(response.data.posts.length).toBe(1);
+  expect(response.data.posts[0].title).toBe('First Post');
+  expect(response.data.posts[0].body).toBe('This is the first post');
+  expect(response.data.posts[0].published).toBe(true);
+});
